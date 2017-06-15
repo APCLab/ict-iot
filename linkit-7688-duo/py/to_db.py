cb3ba72ec9218902cecdc78f161aa05cc2632f65
Iblis Lin <iblis@hs.ntnu.edu.tw>
2017-06-16 01:07:11 +0800

    py: to_db

--- linkit-7688-duo/py/to_db.py
+++ linkit-7688-duo/py/to_db.py
@@ -1,12 +1,10 @@
-'''
-Push values to database
-'''
-
 from __future__ import print_function
 
 import os
 import socket
 import sys
+
+from time import time
 
 import requests
 
@@ -26,11 +24,13 @@ def main():
 
         payload = {
             'char32': socket.gethostname(),
+            'int1': int(time()),  # timestamp
             'float1': float(h),
             'float2': float(t),
             'text': "h, t",
         }
-        requests.put(BASE_URL, auth=('demo', 'demo'), json=json)
+        r = requests.post(BASE_URL, auth=('demo', 'demo'), json=payload)
+        assert r.status_code == 201, r.status_code
 
         print(h, t)
 
